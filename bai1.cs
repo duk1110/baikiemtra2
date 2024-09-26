90bd863321049349ce3958a57229a99b2834dc26
duk1110 <ducong2901@gmail.com>
2024-09-26 15:50:04 +0700

    Add files via upload

---
+++ bai1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; } 
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class Student : Person
+{
+    public string StudentId { get; set; }
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine("StudentID: " + StudentId);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student
+        {
+            Name = "John Doe",
+            Age = 20,
+            StudentId = "S12345",
+        };
+        /*
+        Console.WriteLine("Nhap ten:");
+        student.Name = Console.ReadLine();
+        Console.WriteLine("Nhap tuoi:");
+        student.Age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap ma sinh vien:");
+        student.StudentId = Console.ReadLine();
+        */
+        
+
+        student.ShowInfo();
+    }
+}
---
+++ bai2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+interface IShape
+{
+    double CalculateArea();
+}
+
+class Circle : IShape
+{
+    public double Radius { get; set; }
+    public double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : IShape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle();
+        Console.WriteLine("Nhap Radius:");
+        circle.Radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh tron: " + circle.CalculateArea());
+        Rectangle rectangle = new Rectangle();
+        Console.WriteLine("Nhap Width:");
+        rectangle.Width = double.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap Height:");
+        rectangle.Height = double.Parse(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh chu nhat:" + rectangle.CalculateArea());
+    }
+}
+
